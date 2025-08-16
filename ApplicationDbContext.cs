3e1339bf9c58a79d968a3cdf2ebb18c5781da2cd
Amalia <riclea.amalia@gmail.com>
2025-08-16 16:15:53 +0300

    Add files via upload

---
+++ ApplicationDbContext.cs
@@ -0,0 +1,26 @@
+﻿using Microsoft.EntityFrameworkCore;
+
+public class ApplicationDbContext : DbContext
+{
+    public ApplicationDbContext(DbContextOptions<ApplicationDbContext> options)
+        : base(options)
+    {
+    }
+
+
+    protected override void OnModelCreating(ModelBuilder modelBuilder)
+    {
+        base.OnModelCreating(modelBuilder);
+
+        // Optional: apply configurations
+        modelBuilder.ApplyConfigurationsFromAssembly(typeof(ApplicationDbContext).Assembly);
+
+    }
+
+    public DbSet<Show> Shows { get; set; }
+    //public DbSet<Location> Locations { get; set; }
+    //public DbSet<Category> Categories { get; set; }
+
+
+
+}
